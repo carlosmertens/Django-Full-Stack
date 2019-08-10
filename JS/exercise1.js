664c072bdd37caefe24419939254a82790a982e8
Carlos Mertens <mertenscarlos@yahoo.com>
2019-08-10 22:45:24 +0200

    Create scripts to calculate kilograms from pounds with JS

---
+++ JS/exercise1.js
@@ -0,0 +1,5 @@
+alert("Welcome to kilograms calculator!");
+var pound = prompt("Enter pounds (lbs): ");
+var total = pound * 0.454;
+alert("That is " + total + " kilograms");
+console.log("Convertion Completed");
