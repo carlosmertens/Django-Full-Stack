ff88cf31c388a1b0abfc6520b7c76361c151af30
Carlos Mertens <mertenscarlos@yahoo.com>
2019-09-10 00:07:13 +0200

    Create scripts for Modules and Packages

---
+++ Python-Intermetiate/05_module_packages.py
@@ -0,0 +1,12 @@
+# MODULE AND PACKAGES
+
+# Import 05_module.py
+import modulePackages
+# import modulePackages as mP
+# from modulePackages import func_in_module
+
+# Call the function in the imported script
+# This will create __pycache__ file (Compiler helper)
+
+modulePackages.func_in_module()
+# mP.func_in_module()
---
+++ Python-Intermetiate/modulePackages.py
@@ -0,0 +1,7 @@
+# MODULE AND PACKAGES
+
+# Create a function to be call from 05_module_packages.py
+
+
+def func_in_module():
+    print("*** Function in modulePackages.py script! ***")
