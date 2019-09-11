866b958b5021ee00c5ecef40ecd51573d5bef06b
Carlos Mertens <mertenscarlos@yahoo.com>
2019-09-11 23:11:32 +0200

    Initate again myProject with Django

--- Django/myProject/myProject/urls.py
+++ Django/myProject/myProject/urls.py
@@ -1,4 +1,4 @@
-"""first_project_ever URL Configuration
+"""myProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
