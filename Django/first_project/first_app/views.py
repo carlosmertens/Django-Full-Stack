c1b5250943f8b512c183051e3b19fe04bbde8616
Carlos Mertens <mertenscarlos@yahoo.com>
2019-09-21 14:30:20 +0200

    Inject data to Models-Templates_Views

--- Django/first_project/first_app/views.py
+++ Django/first_project/first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 
@@ -11,5 +12,6 @@ def index(request):
     Use the render() function to return the content of the key call in the tag 
     template 'insert_me'
     """
-    my_dict = {'insert_me': "Hello! I am at first_app/views.py"}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+    return render(request, 'first_app/index.html', context=date_dict)
